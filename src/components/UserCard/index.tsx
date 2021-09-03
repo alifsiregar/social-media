449ae35e69e807399a80f8c84dbb23fd595401ea
Alif Siregar <alifsiregar21@gmail.com>
2021-09-03 18:00:40 +0700

    create usercard component

---
+++ src/components/UserCard/index.tsx
@@ -0,0 +1,13 @@
+import {
+    Container
+} from './UserCard.styles';
+
+const index = () => {
+    return (
+        <Container>
+           <h2>Hello World!</h2> 
+        </Container>
+    )
+}
+
+export default index
