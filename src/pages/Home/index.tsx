b1677e67fb137241e71ed5552983894ab668f523
Alif Siregar <alifsiregar21@gmail.com>
2021-09-03 17:32:47 +0700

    add Home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import {
+    Container
+} from './Home.styles';
+
+const Home = () => {
+    return (
+        <Container>
+            <h1>Home</h1>
+        </Container>
+    )
+}
+
+export default Home
