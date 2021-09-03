c1b4e98e767024c1d86305813873ebd3ab517cdc
Alif Siregar <alifsiregar21@gmail.com>
2021-09-03 17:39:34 +0700

    implement routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
+import { 
+  BrowserRouter as Router,
+  Switch,
+  Route 
+} from 'react-router-dom';
 import { GlobalStyle } from './Global.styles';
+import { routes } from './routes';
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <span>
-        Social Media Skeleton
-      </span>
+      <Router basename={process.env.PUBLIC_URL}>
+        <Switch>
+          {routes.map((route) => (
+            <Route
+              key={route.name}
+              exact={route.exact}
+              path={route.path}
+              component={route.component}
+            />
+          ))}
+        </Switch>
+      </Router>
     </>
   );
 }
