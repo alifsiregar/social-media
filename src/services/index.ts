17bd5ca556e1cc89bf72cbfdd5787f2f329bd481
Alif Siregar <alifsiregar21@gmail.com>
2021-09-04 22:31:31 +0700

    add getuserposts endpoint to services

--- src/services/index.ts
+++ src/services/index.ts
@@ -29,3 +29,18 @@ export const getUserDetail = async (id: string) : Promise<string> => {
     return 'Error';
   }
 };
+
+export const getUserPosts = async (id: string) : Promise<string> => {
+  try {
+    const json = await axios({
+      url: `https://jsonplaceholder.typicode.com/users/${id}/posts`,
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    return json.data;
+  } catch (e) {
+    return 'Error';
+  }
+};
