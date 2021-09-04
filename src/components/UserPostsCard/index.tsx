7a263366bbb1aaec576723725b874a8955d57806
Alif Siregar <alifsiregar21@gmail.com>
2021-09-04 22:32:01 +0700

    add userpostscard component

---
+++ src/components/UserPostsCard/index.tsx
@@ -0,0 +1,27 @@
+import { Link } from 'react-router-dom';
+import {
+    Container,
+    UserInfo,
+    ButtonNav,
+} from './UserPostsCard.styles';
+
+const UserPostsCard : React.FC<{posts :{
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+}}> = ({posts}) => {
+    return (
+        <Container>
+            <UserInfo>
+                <h2>{posts.title}</h2>
+                <span>{posts.body}</span>
+            </UserInfo>
+            <ButtonNav to={`/user/${posts.id}`}>
+                DETAILS
+            </ButtonNav>
+        </Container>
+    )
+}
+
+export default UserPostsCard
