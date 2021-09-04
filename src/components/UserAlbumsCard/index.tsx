ab8ca7c8b75a4adcfd6ff2bcd210b48c0a90d501
Alif Siregar <alifsiregar21@gmail.com>
2021-09-04 23:59:24 +0700

    create useralbumscard component

---
+++ src/components/UserAlbumsCard/index.tsx
@@ -0,0 +1,25 @@
+import { Link } from 'react-router-dom';
+import {
+    Container,
+    UserInfo,
+    ButtonNav,
+} from './UserAlbumsCard.styles';
+
+const UserPostsCard : React.FC<{album :{
+    userId: number;
+    id: number;
+    title: string;
+}}> = ({album}) => {
+    return (
+        <Container>
+            <UserInfo>
+                <h2>{album.title}</h2>
+            </UserInfo>
+            <ButtonNav to={`/user/${album.id}`}>
+                DETAILS
+            </ButtonNav>
+        </Container>
+    )
+}
+
+export default UserPostsCard
