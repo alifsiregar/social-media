7635e2a061abb44bab8e091a59cf43e1ca0673fe
Alif Siregar <alifsiregar21@gmail.com>
2021-09-05 07:52:10 +0700

    finish postdetail page

--- src/pages/PostDetail/PostDetail.styles.ts
+++ src/pages/PostDetail/PostDetail.styles.ts
@@ -1,1 +1,35 @@
 import styled from "styled-components";
+import { Link } from 'react-router-dom';
+import * as Ai from "react-icons/ai";
+
+export const Container = styled.div`
+    margin: 20px;
+    color: #EDEDED;
+    max-width: 1366px;
+    display: flex;
+    flex-direction: column;
+
+    h4{
+        margin: 0;
+    }
+
+    span {
+        text-align: justify;
+    }
+
+    @media only screen and (min-width: 1386px) {
+        margin: 20px auto;
+    }
+`;
+
+export const NavLink = styled(Link)`
+    color: #EDEDED;
+    text-decoration: none;
+    display: flex;
+`;
+
+export const LeftArrow = styled(Ai.AiOutlineArrowLeft)`
+    font-size: 15px;
+    background: none;
+    margin: 0 10px 0 0;
+`;
--- src/pages/PostDetail/index.tsx
+++ src/pages/PostDetail/index.tsx
@@ -1,8 +1,93 @@
-const PostDetail = () => {
+import { useState, useEffect } from 'react';
+import { RouteComponentProps } from 'react-router-dom';
+import {
+    Container,
+    NavLink,
+    LeftArrow
+} from './PostDetail.styles';
+import { 
+    getPostDetail,
+    getPostComments
+} from '../../services';
+import CommentsCard from '../../components/CommentsCard';
+
+const PostDetail = ({ match }: RouteComponentProps<{id:string, postId: string}>) => {
+
+    const [postLoading, setPostLoading] = useState(true);
+    const [commentLoading, setCommentLoading] = useState(true);
+    const [post, setPost] = useState<{
+        userId: number;
+        id: number;
+        title: string;
+        body: string;
+    }>({
+        userId: 0,
+        id: 0,
+        title: '',
+        body: ''
+    });
+    const [comments, setComments] = useState<{
+        postId: number;
+        id: number;
+        email: string;
+        body: string;
+    }[]>([])
+
+    useEffect(() => {
+        const requestData = async () : Promise<void> => {
+            await getPostDetail(match.params.postId).then((res:any) => {
+                setPost(res);
+                setPostLoading(false);
+            });
+            await getPostComments(match.params.postId).then((res:any) => {
+                setComments(res);
+                setCommentLoading(false);
+            })
+        }
+        requestData();
+    }, []);
+
     return (
-        <div>
+        <Container>
+            <NavLink to={`/user/${match.params.id}`}>
+                <LeftArrow />
+                <h4>
+                    Profile
+                </h4>
+            </NavLink>
+
+            {!postLoading ?
+                <> 
+                    <h1>
+                        {post.title}
+                    </h1>
+                    <span className="post-body">
+                        {post.body}
+                    </span>
+                </>
+                :
+                <h1>
+                   Loading Post... 
+                </h1>
+            }
+
+            {!commentLoading ?
+                <>
+                    <h1>
+                        Comments
+                    </h1>
+                    {comments.map((item) => {
+                        return <CommentsCard key={item.id} item={item} />
+                    })}
+                </>
+                    :
+                <h1>
+                    Loading Post... 
+                </h1>
+            }
+
             
-        </div>
+        </Container>
     )
 }
 
