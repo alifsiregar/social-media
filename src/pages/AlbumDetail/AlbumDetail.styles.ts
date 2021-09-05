5ca1b479984f22167ca649ff3a1a151f97f89af9
Alif Siregar <alifsiregar21@gmail.com>
2021-09-05 10:26:13 +0700

    add albumdetail page

--- src/pages/AlbumDetail/AlbumDetail.styles.ts
+++ src/pages/AlbumDetail/AlbumDetail.styles.ts
@@ -1,1 +1,40 @@
 import styled from "styled-components";
+import { Link } from 'react-router-dom';
+import * as Ai from "react-icons/ai";
+
+export const Container = styled.div`
+    margin: 20px;
+    color: #EDEDED;
+    display: flex;
+    justify-content: center;
+
+    h4{
+        margin: 0;
+    }
+`;
+
+export const ContentContainer = styled.div`
+    max-width: 1366px;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+`;
+
+
+export const NavLink = styled(Link)`
+    color: #EDEDED;
+    text-decoration: none;
+    display: flex;
+`;
+
+export const LeftArrow = styled(Ai.AiOutlineArrowLeft)`
+    font-size: 15px;
+    background: none;
+    margin: 0 10px 0 0;
+`;
+
+export const PhotosContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+`;
--- src/pages/AlbumDetail/index.tsx
+++ src/pages/AlbumDetail/index.tsx
@@ -1,8 +1,76 @@
-const AlbumDetail = () => {
+import { useState, useEffect } from "react";
+import { RouteComponentProps } from 'react-router-dom';
+import {
+    Container,
+    ContentContainer,
+    NavLink,
+    LeftArrow,
+    PhotosContainer
+} from './AlbumDetail.styles';
+import PhotosCard from '../../components/PhotosCard';
+import { 
+    getAlbumDetail,
+    getAlbumPhotos
+} from '../../services';
+
+const AlbumDetail = ({ match }: RouteComponentProps<{id:string, albumId: string}>) => {
+
+    const [albumDetailLoading, setAlbumDetailLoading] = useState<boolean>(true);
+    const [albumPhotosLoading, setAlbumPhotosLoading] = useState<boolean>(true);
+    const [albumDetail, setAlbumDetail] = useState<{
+        userId: number;
+        id: number;
+        title: string;
+    }>({
+        userId: 0,
+        id: 0,
+        title: ''
+    });
+    const [albumPhotos, setAlbumPhotos] = useState<{
+        albumId: number;
+        id: number;
+        title: string;
+        url: string;
+        thumbnailUrl: string;
+    }[]>([]);
+
+    useEffect(() => {
+        const requestData = async () : Promise<void> => {
+            await getAlbumDetail(match.params.albumId).then((res:any) => {
+                setAlbumDetail(res);
+                setAlbumDetailLoading(false);
+            });
+            await getAlbumPhotos(match.params.albumId).then((res:any) => {
+                setAlbumPhotos(res);
+                setAlbumPhotosLoading(false);
+            })
+        }
+        requestData();
+    }, []);
+
     return (
-        <div>
-            Album Detail
-        </div>
+        <Container>
+            <ContentContainer>
+                <NavLink to={`/user/${match.params.id}`}>
+                    <LeftArrow />
+                    <h4>
+                        Profile
+                    </h4>
+                </NavLink>
+                <h1>{albumDetail.title}</h1>
+                {!albumPhotosLoading ? 
+                    <PhotosContainer>
+                        {albumPhotos.map((item) => {
+                            return <PhotosCard key={item.id} image={item.thumbnailUrl} title={item.title} />
+                        })}
+                    </PhotosContainer>
+                    :
+                    <h1>
+                        Loading...
+                    </h1>
+                }
+            </ContentContainer>
+        </Container>
     )
 }
 
