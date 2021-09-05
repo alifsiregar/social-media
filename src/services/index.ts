a138fc364ef6a2142ba1c174b1ed64483c32f591
Alif Siregar <alifsiregar21@gmail.com>
2021-09-05 17:09:53 +0700

    add endpoints for comment and post crud

--- src/services/index.ts
+++ src/services/index.ts
@@ -45,6 +45,61 @@ export const getUserPosts = async (id: string) : Promise<string> => {
   }
 };
 
+export const addUserPosts = async (id: string, title: string, body: string) : Promise<string> => {
+  try {
+    const json = await axios({
+      url: `https://jsonplaceholder.typicode.com/users/${id}/posts`,
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: {
+        title: title,
+        body: body,
+      }
+    });
+    return json.data;
+  } catch (e: any) {
+    return e;
+  }
+};
+
+export const deleteUserPosts = async (id: string) : Promise<string> => {
+  try {
+    const json = await axios({
+      url: `https://jsonplaceholder.typicode.com/posts/${id}`,
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    return json.data;
+  } catch (e: any) {
+    return e;
+  }
+};
+
+export const updateUserPosts = async (id: string, title: string, body: string, userId: string) : Promise<string> => {
+  try {
+    const json = await axios({
+      url: `https://jsonplaceholder.typicode.com/posts/${id}`,
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: {
+        id: id,
+        title: title,
+        body: body,
+        userId: userId,
+      }
+    });
+    return json.data;
+  } catch (e: any) {
+    return e;
+  }
+};
+
 export const getUserAlbums = async (id: string) : Promise<string> => {
   try {
     const json = await axios({
@@ -83,6 +138,77 @@ export const getPostComments = async (postId: string) : Promise<string> => {
       headers: {
         'Content-Type': 'application/json',
       },
+    });
+    return json.data;
+  } catch (e: any) {
+    return e;
+  }
+};
+
+export const getCommentDetail = async (commentId: string) : Promise<string> => {
+  try {
+    const json = await axios({
+      url: `https://jsonplaceholder.typicode.com/comments/${commentId}/`,
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    return json.data;
+  } catch (e: any) {
+    return e;
+  }
+};
+
+export const addPostComments = async (id: string, email: string, body: string) : Promise<string> => {
+  try {
+    const json = await axios({
+      url: `https://jsonplaceholder.typicode.com/posts/${id}/comments/`,
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: {
+        email: email,
+        body: body,
+      }
+    });
+    return json.data;
+  } catch (e: any) {
+    return e;
+  }
+};
+
+export const deletePostComments = async (id: string) : Promise<string> => {
+  try {
+    const json = await axios({
+      url: `https://jsonplaceholder.typicode.com/comments/${id}/`,
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    return json.data;
+  } catch (e: any) {
+    return e;
+  }
+};
+
+
+export const updatePostComments = async (id: string, email: string, body: string, postId: string) : Promise<string> => {
+  try {
+    const json = await axios({
+      url: `https://jsonplaceholder.typicode.com/comments/${id}`,
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: {
+        id: id,
+        email: email,
+        body: body,
+        postId: postId,
+      }
     });
     return json.data;
   } catch (e: any) {
